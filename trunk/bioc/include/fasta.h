57175fd1b627a0a3f3f8ba071f9cb77d22d9e6ed
r78v10a07@gmail.com <r78v10a07@gmail.com@207468d6-2d5d-008d-7328-7124a6e6b407>
2014-04-14 18:35:42 +0000

    Initial import

---
+++ trunk/bioc/include/fasta.h
@@ -0,0 +1,109 @@
+/* 
+ * File:   fasta.h
+ * Author: roberto
+ *
+ * Created on April 14, 2014, 11:58 AM
+ */
+
+#ifndef FASTA_H
+#define	FASTA_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    struct fasta_s {
+        /*
+         * Members          
+         */
+        char *header;
+        char *seq;
+
+        /*
+         * Methods
+         */
+
+        /**
+         * Set the fasta header
+         * 
+         * @param self the container object
+         * @param string the header
+         */
+        void (*setHeader)(void *self, char *string);
+
+        /**
+         * Set the sequence
+         * 
+         * @param self the container object
+         * @param string the sequence
+         */
+        void (*setSeq)(void *self, char *string);
+
+        /**
+         * Creates segments of length with an overlap of offset
+         * 
+         * @param self the container object
+         * @param out the output file 
+         * @param length the length of the segments
+         * @param offset the offset of the segments
+         * @param lineLength the length of the fasta line
+         */
+        void (*printOverlapSegments)(void * self, FILE *out, int length, int offset, int lineLength);
+
+        /**
+         * Extract and print a segments from the start position with length
+         * 
+         * @param self the container object
+         * @param out the output file 
+         * @param header the fasta header
+         * @param start the start position 
+         * @param length the segment length
+         * @param lineLength the length of the fasta line
+         */
+        void (*printSegment)(void * self, FILE *out, char *header, int start, int length, int lineLength);
+
+        /**
+         * Print in fasta file to the STDOUT
+         * 
+         * @param self the container object
+         * @param lineLength the length of the fasta line
+         */
+        void (*toString)(void *self, int lineLength);
+
+        /**
+         * Print in fasta format with a line length of lineLength 
+         * 
+         * @param self the container object
+         * @param out the output file
+         * @param lineLength the length of the fasta line
+         */
+        void (*toFile)(void * self, FILE *out, int lineLength);
+
+        /**
+         * Return the length of the fasta sequence
+         * 
+         * @param self the container object
+         * @return the length of the fasta sequence
+         */
+        int (*length)(void *self);
+
+        /**
+         * Free the fasta container
+         * 
+         * @param self the container object
+         */
+        void (*free)(void *self);
+    };
+
+    typedef struct fasta_s *fasta_l;
+
+    extern fasta_l CreateFasta();
+
+    extern fasta_l ReadFasta(FILE *fp);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* FASTA_H */
+
---
+++ trunk/bioc/tests/memorytest.c
@@ -0,0 +1,58 @@
+/*
+ * File:   memorytest.c
+ * Author: roberto
+ *
+ * Created on Apr 14, 2014, 11:55:56 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <CUnit/Basic.h>
+
+/*
+ * CUnit Test Suite
+ */
+
+int init_suite(void) {
+    return 0;
+}
+
+int clean_suite(void) {
+    return 0;
+}
+
+void* allocate(size_t nmemb, size_t size, char* file, int line);
+
+void testAllocate() {    
+    void* result = allocate(1, sizeof(int), __FILE__, __LINE__);
+    if (!result) {
+        CU_ASSERT(0);
+    }
+}
+
+int main() {
+    CU_pSuite pSuite = NULL;
+
+    /* Initialize the CUnit test registry */
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    /* Add a suite to the registry */
+    pSuite = CU_add_suite("memorytest", init_suite, clean_suite);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Add the tests to the suite */
+    if ((NULL == CU_add_test(pSuite, "testAllocate", testAllocate))) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
