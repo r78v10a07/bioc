9234cddad44e8d909ffc3ed4cf6bd0415a551a40
r78v10a07@gmail.com <r78v10a07@gmail.com@207468d6-2d5d-008d-7328-7124a6e6b407>
2014-05-07 15:48:17 +0000

    Changing dir name

--- trunk/biocTaxLineageFromGi/nbproject/Package-Release.bash
+++ trunk/biocTaxLineageFromGi/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/bioctaxonomy
-OUTPUT_BASENAME=bioctaxonomy
-PACKAGE_TOP_DIR=bioctaxonomy/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/bioctaxlineagefromgi
+OUTPUT_BASENAME=bioctaxlineagefromgi
+PACKAGE_TOP_DIR=bioctaxlineagefromgi/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/bioctaxonomy/bin"
+makeDirectory "${NBTMPDIR}/bioctaxlineagefromgi/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/bioctaxonomy.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/bioctaxlineagefromgi.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/bioctaxonomy.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/bioctaxlineagefromgi.tar *
 checkReturnCode
 
 # Cleanup
