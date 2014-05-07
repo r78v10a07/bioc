4d142f3f728f772bccbf6c87f56cd7848cbc480d
r78v10a07@gmail.com <r78v10a07@gmail.com@207468d6-2d5d-008d-7328-7124a6e6b407>
2014-05-07 15:59:20 +0000

    Updating the Netbeans private files

--- BuildBtreeIndexFasta/nbproject/Makefile-impl.mk
+++ BuildBtreeIndexFasta/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=biocBuildBtreeIndexFasta
+PROJECTNAME=BuildBtreeIndexFasta
 
 # Active Configuration
 DEFAULTCONF=Debug
