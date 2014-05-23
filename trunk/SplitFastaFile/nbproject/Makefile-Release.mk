2f7d19ce46567c892de2b94467f11fdc9900787d
r78v10a07@gmail.com <r78v10a07@gmail.com@207468d6-2d5d-008d-7328-7124a6e6b407>
2014-05-23 12:25:40 +0000

    Modifying the SplitFasta to use the GI to TaxId Btree

--- trunk/SplitFastaFile/nbproject/Makefile-Release.mk
+++ trunk/SplitFastaFile/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/split.o \
 	${OBJECTDIR}/src/splitFasta.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/splitfastafile: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/splitfastafile ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/split.o: split.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/split.o split.c
 
 ${OBJECTDIR}/src/splitFasta.o: src/splitFasta.c 
 	${MKDIR} -p ${OBJECTDIR}/src
