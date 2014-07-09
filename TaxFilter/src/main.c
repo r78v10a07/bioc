c5db785e7a12c0dbd7135bdea48db199abfc6164
r78v10a07@gmail.com <r78v10a07@gmail.com@207468d6-2d5d-008d-7328-7124a6e6b407>
2014-07-09 13:15:05 +0000

    Modifying the filtering function to increase the speed

--- TaxFilter/src/main.c
+++ TaxFilter/src/main.c
@@ -12,6 +12,7 @@
 #include <string.h>
 #include <getopt.h>
 #include <stdbool.h>
+#include <pthread.h>
 #include <zlib.h>
 #include <time.h>
 #include "btree.h"
@@ -23,6 +24,17 @@
 #include "fasta.h"
 
 char *program_name;
+
+typedef struct thread_param {
+    int number;
+    char *out;
+    char *in;
+    off_t start;
+    off_t end;
+    BtreeNode_t *taxIn;
+    BtreeNode_t *gi_tax;
+    int verbose;
+} thread_param_t;
 
 void print_usage(FILE *stream, int exit_code) {
     fprintf(stream, "\n********************************************************************************\n");
@@ -36,7 +48,7 @@ void print_usage(FILE *stream, int exit_code) {
     fprintf(stream, "-d,   --dir                         NCBI Taxonomy db dir\n");
     fprintf(stream, "-s,   --skip                        File with the TaxId to skip\n");
     fprintf(stream, "-i,   --include                     File with the TaxId to include. All children will be included\n");
-    fprintf(stream, "-l,   --line                        The fasta line size (default: 80)\n");
+    fprintf(stream, "-p,   --threads                     NUmber of threads (default: 2)\n");
     fprintf(stream, "********************************************************************************\n");
     fprintf(stream, "\n            Roberto Vera Alvarez (e-mail: r78v10a07@gmail.com)\n\n");
     fprintf(stream, "********************************************************************************\n");
@@ -122,30 +134,115 @@ BtreeNode_t *TaxsToInclude(char *dirName, char *include, char *skip, int verbose
     return taxIn;
 }
 
+void *pthreadTaxFilter(void *arg) {
+    thread_param_t *parms = ((thread_param_t*) arg);
+
+    FILE *fd1 = checkPointerError(fopen(parms->in, "r"), "Can't open include file", __FILE__, __LINE__, -1);
+    FILE *fd2 = checkPointerError(fopen(parms->out, "w"), "Can't open include file", __FILE__, __LINE__, -1);
+    BtreeNode_t *taxIn = parms->taxIn;
+    BtreeNode_t *gi_tax = parms->gi_tax;
+    BtreeRecord_t *rec;
+
+    int bufferSize, excludeSeq, gi, ends;
+    size_t buf_size;
+    char *buffer, *line, *str;
+    int numLines, header;
+
+    off_t pos;
+
+    pos = parms->start;
+    fseeko(fd1, parms->start, SEEK_SET);
+
+    bufferSize = 8000000;
+    if (parms->end - parms->start < bufferSize)
+        bufferSize = parms->end - parms->start;
+
+    excludeSeq = 1;
+    buf_size = sizeof (char) * bufferSize;
+    buffer = allocate(sizeof (char) * (bufferSize + 1), __FILE__, __LINE__);
+    numLines = ends = header = 0;
+    gi = -1;
+    while (!feof(fd1)) {
+        memset(buffer, 0, buf_size);
+        if (fread(buffer, buf_size, 1, fd1) == 1) {
+            buffer[bufferSize] = '\0';
+        }
+        str = buffer;
+        while (1) {
+            line = strchr(str, '\n');
+            if (line) *line = '\0';
+            if (*str != '\0') {
+                if (*str == '>' || header) {
+                    if (ends == 2) {
+                        ends = 3;
+                        break;
+                    }
+                    if (!header) {
+                        excludeSeq = 1;
+                        sscanf(str, ">gi|%d|", &gi);
+                        if ((rec = BTreeFind(gi_tax, gi, false)) != NULL) {
+                            if ((rec = BTreeFind(taxIn, *((int *) rec->value), false)) != NULL) {
+                                fprintf(fd2, ">%d;%d\n", gi, *((int *) rec->value));
+                                excludeSeq = 0;
+                            }
+                        }
+                    }
+                    if (line) {
+                        header = 0;
+                    } else {
+                        header = 1;
+                    }
+                } else {
+                    if (gi != -1 && !excludeSeq) {
+                        fprintf(fd2, "%s", str);
+                        if (line) {
+                            fprintf(fd2, "\n");
+                        }
+                    }
+                }
+            }
+            if (!line) break;
+            str = line + 1;
+            numLines++;
+        }
+        if (ends == 1) ends = 2;
+        pos = ftello(fd1);
+        if (buf_size >= parms->end - pos && ends == 0) {
+            bufferSize = parms->end - pos;
+            buf_size = sizeof (char) * bufferSize;
+            ends = 1;
+        }
+        if (ends == 3) break;
+    }
+    free(buffer);
+    fclose(fd1);
+    fclose(fd2);
+    return NULL;
+}
+
 /*
  * 
  */
 int main(int argc, char** argv) {
     struct timespec start, stop, mid;
-    int next_option, verbose, count, lineSize, gi, countSeq;
-    const char* const short_options = "vhn:o:t:d:s:i:";
+    int i, next_option, verbose, count, pthreads;
+    const char* const short_options = "vhn:o:t:d:s:i:p:";
     char *ntName, *output, *taxgiName, *tmp, *dirName, *skipName, *includeName;
 
     FILE *fd1, *fd2;
-    off_t pos, tot;
-    float percent;
+    off_t tot, perThread;
+
+    pthread_t *threads;
+    thread_param_t *tp;
+    int thread_join_res;
 
     BtreeNode_t *taxIn = NULL;
     BtreeNode_t *gi_tax = NULL;
-    BtreeRecord_t *rec;
-
-    int bufferSize, excludeSeq;
-    size_t buf_size, seq_size;
-    char *buffer, *seq, *line, *str;
-    int numLines, readTotal;
-
-    fasta_l self = NULL;
     long long int countWords;
+
+    int bufferSize;
+    size_t buf_size;
+    char *buffer, *line, *str;
 
     clock_gettime(CLOCK_MONOTONIC, &start);
     program_name = argv[0];
@@ -159,10 +256,11 @@ int main(int argc, char** argv) {
         { "dir", 1, NULL, 'd'},
         { "skip", 1, NULL, 's'},
         { "include", 1, NULL, 'i'},
+        { "threads", 1, NULL, 'p'},
         { NULL, 0, NULL, 0} /* Required at end of array.  */
     };
 
-    lineSize = 80;
+    pthreads = 2;
     verbose = 0;
     ntName = output = taxgiName = dirName = skipName = includeName = NULL;
     do {
@@ -200,8 +298,8 @@ int main(int argc, char** argv) {
                 includeName = strdup(optarg);
                 break;
 
-            case 'l':
-                lineSize = atoi(optarg);
+            case 'p':
+                pthreads = atoi(optarg);
                 break;
         }
     } while (next_option != -1);
@@ -209,6 +307,11 @@ int main(int argc, char** argv) {
     if (!dirName || !output || !ntName || !taxgiName || !includeName) {
         print_usage(stderr, -1);
     }
+
+    if (pthreads < 2) pthreads = 2;
+
+    threads = allocate(sizeof (pthread_t) * pthreads, __FILE__, __LINE__);
+    tp = allocate(sizeof (thread_param_t) * pthreads, __FILE__, __LINE__);
 
     taxIn = TaxsToInclude(dirName, includeName, skipName, verbose);
 
@@ -223,7 +326,7 @@ int main(int argc, char** argv) {
         printf("%.1f sec\n", timespecDiffSec(&stop, &mid));
         fflush(stdout);
     }
-
+     
     count = countWords = 0;
     fd1 = checkPointerError(fopen(ntName, "r"), "Can't open include file", __FILE__, __LINE__, -1);
     tmp = allocate(sizeof (char) * (strlen(output) + 100), __FILE__, __LINE__);
@@ -234,110 +337,89 @@ int main(int argc, char** argv) {
     fseeko(fd1, 0, SEEK_END);
     tot = ftello(fd1);
     fseeko(fd1, 0, SEEK_SET);
+    fclose(fd1);
 
-    countSeq = 0;
-    pos = 0;
-    bufferSize = 7000000;
-    excludeSeq = 1;
-    seq_size = buf_size = sizeof (char) * bufferSize;
+    perThread = tot / pthreads;
+
+    if (verbose)
+        printf("Fasta file's size: %lu\tSplit in: %lu\n", tot, perThread);
+
+    for (i = 0; i < pthreads; i++) {
+        tp[i].number = i;
+        tp[i].out = allocate(sizeof (char) * (strlen(output) + 20), __FILE__, __LINE__);
+        sprintf(tp[i].out, "%s_%d_p.fasta", output, i);
+        tp[i].in = ntName;
+        tp[i].gi_tax = gi_tax;
+        tp[i].taxIn = taxIn;
+        tp[i].verbose = verbose;
+        tp[i].start = i * perThread;
+        if (i == pthreads - 1) {
+            tp[i].end = tot;
+        } else {
+            tp[i].end = (i + 1) * perThread;
+        }
+        pthread_create(&threads[i], NULL, pthreadTaxFilter, (void*) &(tp[i]));
+    }
+
+    bufferSize = 8000000;
+    buf_size = sizeof (char) * bufferSize;
     buffer = allocate(sizeof (char) * (bufferSize + 1), __FILE__, __LINE__);
-    seq = allocate(sizeof (char) * (bufferSize + 1), __FILE__, __LINE__);
-    memset(seq, 0, seq_size);
-    readTotal = 0;
-    numLines = 0;
-    while (!feof(fd1)) {
-        memset(buffer, 0, buf_size);
-        if (fread(buffer, buf_size, 1, fd1) == 1) buffer[bufferSize] = '\0';
-        str = buffer;
-        while (1) {
-            line = strchr(str, '\n');
-            if (line) *line = '\0';
-            if (*str != '\0' && *str != '\n') {
-                if (*str == '>') {
-                    if (self != NULL) {
-                        if (!excludeSeq) {
-                            self->seq = seq;
-                            self->len = strlen(seq);
-                            countWords += (self->len + strlen(self->header) + 3);
+
+    for (i = 0; i < pthreads; i++) {
+        thread_join_res = pthread_join(threads[i], NULL);
+        if (thread_join_res != 0) {
+            checkPointerError(NULL, "JOIN ERROR", __FILE__, __LINE__, -1);
+        } else {
+            printf("Thread %d ends\n", i);
+            fd1 = checkPointerError(fopen(tp[i].out, "r"), "Can't open include file", __FILE__, __LINE__, -1);
+            while (!feof(fd1)) {
+                memset(buffer, 0, buf_size);
+                if (fread(buffer, buf_size, 1, fd1) == 1) {
+                    buffer[bufferSize] = '\0';
+                }
+
+                str = buffer;
+                while (1) {
+                    line = strchr(str, '\n');
+                    if (line) *line = '\0';
+                    if (*str != '\0') {
+                        if (*str == '>') {
                             if (countWords > 4294967296) {
                                 count++;
-                                countWords = self->len;
+                                countWords = 0;
                                 fclose(fd2);
                                 sprintf(tmp, "%s_%d.fasta", output, count);
                                 if (verbose) printf("Creating a new file: %s\n", tmp);
                                 fd2 = checkPointerError(fopen(tmp, "w"), "Can't open output file", __FILE__, __LINE__, -1);
                             }
-                            countSeq++;
-                            self->toFile(self, fd2, lineSize);
-                            self->seq = NULL;
-                        }
-                        readTotal = 0;
-                        seq[0] = '\0';
-                        self->free(self);
-                    }
-
-                    readTotal += strlen(str);
-                    self = CreateFasta();
-                    self->header = strndup(str + 1, strlen(str) - 1);
-                    self->getGi(self, &gi);
-                    excludeSeq = 1;
-                    if ((rec = BTreeFind(gi_tax, gi, false)) != NULL) {
-                        if ((rec = BTreeFind(taxIn, *((int *) rec->value), false)) != NULL) {
-                            sprintf(self->header, "%d;%d", gi, *((int *) rec->value));
-                            excludeSeq = 0;
+                            countWords += strlen(str);
+                            fprintf(fd2, "%s", str);
+                            if (line) {
+                                countWords++;
+                                fprintf(fd2, "\n");
+                            }
+                        } else {
+                            countWords += strlen(str);
+                            fprintf(fd2, "%s", str);
+                            if (line) {
+                                countWords++;
+                                fprintf(fd2, "\n");
+                            }
                         }
                     }
-                } else {
-                    checkPointerError(self, "The fasta file does not start with the header (>)", __FILE__, __LINE__, -1);
-                    readTotal += strlen(str);
-                    if (!excludeSeq) {
-                        if (readTotal >= seq_size) {
-                            seq_size += seq_size;
-                            seq = reallocate(seq, seq_size, __FILE__, __LINE__);
-                        }
-                        strcat(seq, str);
-                    }
+                    if (!line) break;
+                    str = line + 1;
                 }
             }
-            if (!line) break;
-            str = line + 1;
-            numLines++;
+            fclose(fd1);
         }
-        pos= ftello(fd1);
-        percent = (float) (pos * 100) / tot;
-        if (verbose) {
-            printf("\t%12d\t%6.2f%%\r", countSeq, percent);
-        }
+        free(tp[i].out);
     }
-    if (self != NULL) {
-        if (!excludeSeq) {
-            self->setSeq(self, seq);
-            countWords += (self->length(self) + strlen(self->header) + 3);
-            if (countWords > 4294967296) {
-                count++;
-                countWords = self->length(self);
-                fclose(fd2);
-                sprintf(tmp, "%s_%d.fasta", output, count);
-                if (verbose) printf("Creating a new file: %s\n", tmp);
-                fd2 = checkPointerError(fopen(tmp, "w"), "Can't open output file", __FILE__, __LINE__, -1);
-            }
-            countSeq++;
-            self->toFile(self, fd2, lineSize);
-        }
-        readTotal = 0;
-        seq[0] = '\0';
-        self->free(self);
-    }
-    if (verbose) {
-        printf("\t%12d\t%6.2f%% \n", countSeq, atof("100.00"));
-    }
-
-    free(tmp);
-    free(seq);
-    free(buffer);
+    free(tp);
+    if (threads) free(threads);
+    if (tmp) free(tmp);
     BTreeFree(gi_tax, free);
     BTreeFree(taxIn, free);
-    if (fd1) fclose(fd1);
     if (fd2) fclose(fd2);
     if (dirName) free(dirName);
     if (output) free(output);
